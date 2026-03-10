e08671b47287528e1bc97193aefef78a02ccedaf
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-09 23:38:40 -0700

    add: koanf select solver

--- koanf/solvers/solver.go
+++ koanf/solvers/solver.go
@@ -10,6 +10,12 @@ type ConfigSolver interface {
 	Solve(config *koanf.Koanf) *koanf.Koanf
 }
 
+// ErrorReporter is an optional extension for solvers that can surface
+// recoverable solver errors to container orchestration code.
+type ErrorReporter interface {
+	Err() error
+}
+
 func ToString(v any) string {
 	return fmt.Sprint(v)
 }
